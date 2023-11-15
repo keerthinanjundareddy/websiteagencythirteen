cd811b6c1cef11a098c5346f93d66257a53ce0dc
keerthinanjundareddy <keerthana@apprikart.com>
2023-11-15 12:47:02 +0530

    me

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import React, { Component } from "react";
-import routes from "../src/routes";
-import {
-  withRouter,
-  Route,
-  Switch,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import "./assets/css/materialdesignicons.min.css";
-
-import "./assets/scss/themes.scss";
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <Switch>
-            {routes.map((route, idx) => (
-              <Route path={route.path} component={route.component} key={idx} />
-            ))}
-          </Switch>
-        </Router>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter(App);
+export default App;
